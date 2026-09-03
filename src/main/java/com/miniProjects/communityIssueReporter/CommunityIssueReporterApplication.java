aef89b15b1e33d4a89649b6a0d9b801d9d804a50
dejoie <sangwadejoie35@gmail.com>
2026-09-03 15:52:43 +0200

    first commit just the files and folders right from the extracted spring initializer

---
+++ src/main/java/com/miniProjects/communityIssueReporter/CommunityIssueReporterApplication.java
@@ -0,0 +1,13 @@
+package com.miniProjects.communityIssueReporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityIssueReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityIssueReporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miniProjects/communityIssueReporter/CommunityIssueReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniProjects.communityIssueReporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityIssueReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
