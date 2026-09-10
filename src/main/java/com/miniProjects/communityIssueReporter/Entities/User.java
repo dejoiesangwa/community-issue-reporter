96ba154f863d6055cd93528e27a924aea1007b45
dejoie <sangwadejoie35@gmail.com>
2026-09-10 13:49:16 +0200

    renamed the entity user

--- src/main/java/com/miniProjects/communityIssueReporter/Entities/User.java
+++ src/main/java/com/miniProjects/communityIssueReporter/Entities/User.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="user")
+@Table(name="User")
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.SEQUENCE)
